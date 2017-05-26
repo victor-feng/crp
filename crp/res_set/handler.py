79f9657d5a90c5e602e1a97f636fc93f846fc8bc
郭威 <guowei5@syswin.com>
2017-05-26 14:31:29 +0800

    first call res_callback ok

--- crp/res_set/handler.py
+++ crp/res_set/handler.py
@@ -41,8 +41,9 @@ FLAVOR_MINI = 'aadb6488-a64f-4868-82dd-7b832cd047ac'
 FLAVOR_1C2G = 'scm2-dev--1C2G80G'
 # docker-2C4G25G
 DOCKER_FLAVOR_2C4G = 'e90d8d25-c5c7-46d7-ba4e-2465a5b1d266'
-# AVAILABILITY_ZONE = 'AZ_GENERAL'
-AVAILABILITY_ZONE = 'AZ-SELF-SERVICE'
+# AVAILABILITY_ZONE
+AVAILABILITY_ZONE_GENERAL = 'AZ_GENERAL'
+AVAILABILITY_ZONE_SELF_SERVICE = 'AZ-SELF-SERVICE'
 DEV_NETWORK_ID = 'c12740e6-33c8-49e9-b17d-6255bb10cd0c'
 
 # res_callback
@@ -82,7 +83,8 @@ def create_instance_by_type(ins_type, name):
     image = images_dict.get(ins_type)
     image_uuid = image.get('uuid')
     Log.logger.debug("Select Image UUID: "+image_uuid+" by Instance Type "+ins_type)
-    return _create_instance(name, image_uuid, FLAVOR_1C2G, AVAILABILITY_ZONE, DEV_NETWORK_ID)
+    return _create_instance(name, image_uuid, FLAVOR_1C2G, AVAILABILITY_ZONE_SELF_SERVICE, DEV_NETWORK_ID)
+    # return _create_instance(name, image_uuid, FLAVOR_1C2G, AVAILABILITY_ZONE_GENERAL, DEV_NETWORK_ID)
 
 
 # 依据镜像URL创建NovaDocker容器
@@ -90,14 +92,14 @@ def create_docker_by_url(name, image_url):
     err_msg, image_uuid = image_transit(image_url)
     if err_msg is None:
         Log.logger.debug("Transit harbor docker image success. The result glance image UUID is " + image_uuid)
-        return None, _create_instance(name, image_uuid, DOCKER_FLAVOR_2C4G, AVAILABILITY_ZONE, DEV_NETWORK_ID)
+        return None, _create_instance(name, image_uuid, DOCKER_FLAVOR_2C4G, AVAILABILITY_ZONE_GENERAL, DEV_NETWORK_ID)
     else:
         return err_msg, None
 
 
 # 申请资源定时任务
 def _create_resource_set(task_id=None, resource_list=None, compute_list=None):
-    osins_id_list = []
+    uop_os_inst_id_list = []
     for resource in resource_list:
         ins_name = resource.get('res_name')
         ins_id = resource.get('res_id')
@@ -109,9 +111,13 @@ def _create_resource_set(task_id=None, resource_list=None, compute_list=None):
         quantity = resource.get('quantity')
         version = resource.get('version')
 
-        for i in range(1, quantity, 1):
+        for i in range(1, quantity+1, 1):
             osint_id = create_instance_by_type(ins_type, ins_name)
-            osins_id_list.append(osint_id)
+            uopinst_info = {
+                               'uop_inst_id': ins_id,
+                               'os_inst_id': osint_id
+                           }
+            uop_os_inst_id_list.append(uopinst_info)
 
     for compute in compute_list:
         ins_name = compute.get('ins_name')
@@ -123,15 +129,19 @@ def _create_resource_set(task_id=None, resource_list=None, compute_list=None):
 
         err_msg, osint_id = create_docker_by_url(ins_name, url)
         if err_msg is None:
-            osins_id_list.append(osint_id)
+            uopinst_info = {
+                   'uop_inst_id': ins_id,
+                   'os_inst_id': osint_id
+               }
+            uop_os_inst_id_list.append(uopinst_info)
         else:
             Log.logger.debug(err_msg)
             result_inst_id_list = []
             # 删除全部
-            _rollback_all(task_id, osins_id_list, result_inst_id_list)
-            osins_id_list = []
+            _rollback_all(task_id, uop_os_inst_id_list, result_inst_id_list)
+            uop_os_inst_id_list = []
 
-    return osins_id_list
+    return uop_os_inst_id_list
 
 
 def _get_ip_from_instance(server):
@@ -146,26 +156,37 @@ def _get_ip_from_instance(server):
 
 
 # 回滚删除全部资源和容器
-def _rollback_all(task_id, osins_id_list, result_inst_id_list):
+def _rollback_all(task_id, uop_os_inst_id_list, result_uop_os_inst_id_list):
     nova_client = OpenStack.nova_client
-    fail_list = list(set(osins_id_list) - set(result_inst_id_list))
+    # fail_list = list(set(uop_os_inst_id_list) - set(result_uop_os_inst_id_list))
+    fail_list = _uop_os_list_sub(uop_os_inst_id_list, result_uop_os_inst_id_list)
     Log.logger.debug("Task ID " + task_id.__str__() + " have one or more instance create failed." +
-                     " Successful instance id set is " + result_inst_id_list[:].__str__() +
+                     " Successful instance id set is " + result_uop_os_inst_id_list[:].__str__() +
                      " Failed instance id set is " + fail_list[:].__str__())
     # 删除全部，完成rollback
-    for inst_id in osins_id_list:
-        nova_client.servers.delete(inst_id)
+    for uop_os_inst_id in uop_os_inst_id_list:
+        nova_client.servers.delete(uop_os_inst_id['os_inst_id'])
     Log.logger.debug("Task ID " + task_id.__str__() + " rollback done.")
 
 
+# _uop_os_list_sub
+def _uop_os_list_sub(uop_os_inst_id_list, result_uop_os_inst_id_list):
+    uop_os_inst_id_wait_query = copy.deepcopy(uop_os_inst_id_list)
+    for i in result_uop_os_inst_id_list:
+        for j in uop_os_inst_id_wait_query:
+            if j['os_inst_id'] == i['os_inst_id']:
+                uop_os_inst_id_wait_query.remove(j)
+    return uop_os_inst_id_wait_query
+
+
 # 向OpenStack查询已申请资源的定时任务
-def _query_resource_set_status(task_id=None, result_list=None, osins_id_list=None, req_dict=None):
+def _query_resource_set_status(task_id=None, result_list=None, uop_os_inst_id_list=None, req_dict=None):
     if result_list.__len__() == 0:
-        result_inst_id_list = []
+        result_uop_os_inst_id_list = []
         result_info_list = []
         result_inst_id_dict = {
                                   'type': 'id',
-                                  'list': result_inst_id_list
+                                  'list': result_uop_os_inst_id_list
                               }
         result_info_dict = {
                                'type': 'info',
@@ -176,47 +197,49 @@ def _query_resource_set_status(task_id=None, result_list=None, osins_id_list=Non
     else:
         for res_dict in result_list:
             if res_dict['type'] == 'id':
-                result_inst_id_list = res_dict['list']
+                result_uop_os_inst_id_list = res_dict['list']
             elif res_dict['type'] == 'info':
                 result_info_list = res_dict['list']
 
     rollback_flag = False
-    os_inst_id_wait_query = list(set(osins_id_list) - set(result_inst_id_list))
-    Log.logger.debug("Query Task ID "+task_id.__str__()+", remain "+os_inst_id_wait_query[:].__str__())
-    Log.logger.debug("Test Task Scheduler Class result_inst_id_list object id is " + id(result_inst_id_list).__str__() +
-                     ", Content is " + result_inst_id_list[:].__str__())
+    # uop_os_inst_id_wait_query = list(set(uop_os_inst_id_list) - set(result_uop_os_inst_id_list))
+    uop_os_inst_id_wait_query = _uop_os_list_sub(uop_os_inst_id_list, result_uop_os_inst_id_list)
+
+    Log.logger.debug("Query Task ID "+task_id.__str__()+", remain "+uop_os_inst_id_wait_query[:].__str__())
+    Log.logger.debug("Test Task Scheduler Class result_uop_os_inst_id_list object id is " + id(result_uop_os_inst_id_list).__str__() +
+                     ", Content is " + result_uop_os_inst_id_list[:].__str__())
     nova_client = OpenStack.nova_client
-    for os_inst_id in os_inst_id_wait_query:
-        inst = nova_client.servers.get(os_inst_id)
-        Log.logger.debug("Task ID "+task_id.__str__()+" query Instance ID "+os_inst_id+" Status is "+inst.status)
+    for uop_os_inst_id in uop_os_inst_id_wait_query:
+        inst = nova_client.servers.get(uop_os_inst_id['os_inst_id'])
+        Log.logger.debug("Task ID "+task_id.__str__()+" query Instance ID "+uop_os_inst_id['os_inst_id']+" Status is "+inst.status)
         if inst.status == 'ACTIVE':
             _ips = _get_ip_from_instance(inst)
             _data = {
-                        'uop_ins_id': req_dict["ins_id"],
-                        'os_inst_id': os_inst_id,
+                        'uop_inst_id': uop_os_inst_id['uop_inst_id'],
+                        'os_inst_id': uop_os_inst_id['os_inst_id'],
                         'ip': _ips.pop() if _ips.__len__() >= 1 else '',
                     }
             result_info_list.append(_data)
             Log.logger.debug("Instance Info: " + _data.__str__())
-            result_inst_id_list.append(os_inst_id)
+            result_uop_os_inst_id_list.append(uop_os_inst_id)
         if inst.status == 'ERROR':
             # 置回滚标志位
             Log.logger.debug("ERROR Instance Info: " + inst.to_dict().__str__())
             rollback_flag = True
 
-    if result_inst_id_list.__len__() == osins_id_list.__len__():
+    if result_uop_os_inst_id_list.__len__() == uop_os_inst_id_list.__len__():
         # TODO(thread exit): 执行成功调用UOP CallBack停止定时任务退出任务线程
         Log.logger.debug("Task ID " + task_id.__str__() + " all instance create success." +
-                         " instance id set is " + result_inst_id_list[:].__str__() +
+                         " instance id set is " + result_uop_os_inst_id_list[:].__str__() +
                          " instance info set is "+result_info_list[:].__str__())
         for info in result_info_list:
-            if info["uop_ins_id"] == req_dict["container_ins_id"]:
+            if info["uop_inst_id"] == req_dict["container_inst_id"]:
                 req_dict["container_ip"] = info["ip"]
-            if info["uop_ins_id"] == req_dict["mysql_ins_id"]:
+            if info["uop_inst_id"] == req_dict["mysql_inst_id"]:
                 req_dict["mysql_ip"] = info["ip"]
-            if info["uop_ins_id"] == req_dict["redis_ins_id"]:
+            if info["uop_inst_id"] == req_dict["redis_inst_id"]:
                 req_dict["redis_ip"] = info["ip"]
-            if info["uop_ins_id"] == req_dict["mongodb_ins_id"]:
+            if info["uop_inst_id"] == req_dict["mongodb_inst_id"]:
                 req_dict["mongodb_ip"] = info["ip"]
         request_res_callback(req_dict)
         Log.logger.debug("Call UOP CallBack Post Success Info.")
@@ -225,7 +248,7 @@ def _query_resource_set_status(task_id=None, result_list=None, osins_id_list=Non
     # 回滚全部资源和容器
     if rollback_flag:
         # 删除全部
-        _rollback_all(task_id, osins_id_list, result_inst_id_list)
+        _rollback_all(task_id, uop_os_inst_id_list, result_uop_os_inst_id_list)
 
         # TODO(thread exit): 执行失败调用UOP CallBack停止定时任务退出任务线程
         Log.logger.debug("Call UOP CallBack Post Fail Info.")
@@ -300,7 +323,7 @@ def request_res_callback(req_dict):
     container["stardand_ins"] = req_dict["stardand_ins"]
     container["cpu"] = req_dict["cpu"]
     container["memory"] = req_dict["memory"]
-    container["ins_id"] = req_dict["ins_id"]
+    container["ins_id"] = req_dict["container_inst_id"]
     project_name["container"] = container
 
     db_info = {}
@@ -335,6 +358,7 @@ def request_res_callback(req_dict):
     data["project_name_list"] = project_name_list
 
     data_str = json.dumps(data)
+    Log.logger.debug("UOP res_callback Request Body is: "+data_str)
     res = requests.post(RES_CALLBACK, data=data_str)
     ret = eval(res.content.decode('unicode_escape'))
     Log.logger.debug(res.status_code)
@@ -390,6 +414,12 @@ class ResourceSet(Resource):
             disk = resource.get('disk')
             quantity = resource.get('quantity')
             version = resource.get('version')
+            if ins_type == 'mysql':
+                req_dict["mysql_inst_id"] = ins_id
+            if ins_type == 'redis':
+                req_dict["redis_inst_id"] = ins_id
+            if ins_type == 'mongo':
+                req_dict["mongodb_inst_id"] = ins_id
 
         for compute in compute_list:
             ins_name = compute.get('ins_name')
@@ -398,6 +428,12 @@ class ResourceSet(Resource):
             cpu = compute.get('cpu')
             mem = compute.get('mem')
             url = compute.get('url')
+            req_dict["container_name"] = ins_name
+            req_dict["image_addr"] = url
+            req_dict["stardand_ins"] = "2C4G"
+            req_dict["cpu"] = cpu
+            req_dict["memory"] = mem
+            req_dict["container_inst_id"] = ins_id
 
         Log.logger.debug(resource_list)
         Log.logger.debug(compute_list)
@@ -407,12 +443,8 @@ class ResourceSet(Resource):
         req_dict["under_name"] = project
         req_dict["resource_id"] = res_id
         req_dict["domain"] = domain
-        req_dict["container_name"] = ins_name
-        req_dict["image_addr"] = url
-        req_dict["stardand_ins"] = "2C4G"
-        req_dict["cpu"] = cpu
-        req_dict["memory"] = mem
-        req_dict["ins_id"] = ins_id
+
+        # init default data
         req_dict["mysql_username"] = "root"
         req_dict["mysql_password"] = "123456"
         req_dict["mysql_port"] = "3306"
@@ -426,8 +458,8 @@ class ResourceSet(Resource):
         req_dict["mongodb_port"] = "27017"
         req_dict["mongodb_ip"] = IP_NONE
 
-        osins_id_list = _create_resource_set(res_id, resource_list, compute_list)
-        if osins_id_list.__len__() == 0:
+        uop_os_inst_id_list = _create_resource_set(res_id, resource_list, compute_list)
+        if uop_os_inst_id_list.__len__() == 0:
             # TODO(callback): 执行失败调用UOP CallBack
             pass
         else:
@@ -435,7 +467,7 @@ class ResourceSet(Resource):
             result_list = []
             Log.logger.debug("Test API handler result_list object id is " + id(result_list).__str__() +
                              ", Content is " + result_list[:].__str__())
-            TaskManager.task_start(SLEEP_TIME, TIMEOUT, result_list, _query_resource_set_status, osins_id_list, req_dict)
+            TaskManager.task_start(SLEEP_TIME, TIMEOUT, result_list, _query_resource_set_status, uop_os_inst_id_list, req_dict)
 
         # return http code 202 (Accepted)
         res_id = 'testid'
