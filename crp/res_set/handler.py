cb4942a5a48f3a28877d4e5be883190557feb5ed
郭威 <guowei5@syswin.com>
2017-05-26 16:51:26 +0800

    fix api, and request api for res_callback

--- crp/res_set/handler.py
+++ crp/res_set/handler.py
@@ -52,6 +52,12 @@ RES_CALLBACK = 'http://uop-test.syswin.com/api/res_callback/res'
 # use localhost for ip is none
 IP_NONE = "localhost"
 
+STATUS_OK = "ok"
+STATUS_FAIL = "fail"
+
+DEFAULT_USERNAME = "root"
+DEFAULT_PASSWORD = "123456"
+
 
 # 向OpenStack申请资源
 def _create_instance(name, image, flavor, availability_zone, network_id):
@@ -98,13 +104,12 @@ def create_docker_by_url(name, image_url):
 
 
 # 申请资源定时任务
-def _create_resource_set(task_id=None, resource_list=None, compute_list=None):
+def _create_resource_set(resource_id=None, resource_list=None, compute_list=None):
     uop_os_inst_id_list = []
     for resource in resource_list:
-        ins_name = resource.get('res_name')
-        ins_id = resource.get('res_id')
-        # ins_id = str(uuid.uuid1())
-        ins_type = resource.get('res_type')
+        instance_name = resource.get('instance_name')
+        instance_id = resource.get('instance_id')
+        instance_type = resource.get('instance_type')
         cpu = resource.get('cpu')
         mem = resource.get('mem')
         disk = resource.get('disk')
@@ -112,25 +117,24 @@ def _create_resource_set(task_id=None, resource_list=None, compute_list=None):
         version = resource.get('version')
 
         for i in range(1, quantity+1, 1):
-            osint_id = create_instance_by_type(ins_type, ins_name)
+            osint_id = create_instance_by_type(instance_type, instance_name)
             uopinst_info = {
-                               'uop_inst_id': ins_id,
+                               'uop_inst_id': instance_id,
                                'os_inst_id': osint_id
                            }
             uop_os_inst_id_list.append(uopinst_info)
 
     for compute in compute_list:
-        ins_name = compute.get('ins_name')
-        ins_id = compute.get('ins_id')
-        # ins_id = str(uuid.uuid1())
+        instance_name = compute.get('instance_name')
+        instance_id = compute.get('instance_id')
         cpu = compute.get('cpu')
         mem = compute.get('mem')
-        url = compute.get('url')
+        image_url = compute.get('image_url')
 
-        err_msg, osint_id = create_docker_by_url(ins_name, url)
+        err_msg, osint_id = create_docker_by_url(instance_name, image_url)
         if err_msg is None:
             uopinst_info = {
-                   'uop_inst_id': ins_id,
+                   'uop_inst_id': instance_id,
                    'os_inst_id': osint_id
                }
             uop_os_inst_id_list.append(uopinst_info)
@@ -138,7 +142,7 @@ def _create_resource_set(task_id=None, resource_list=None, compute_list=None):
             Log.logger.debug(err_msg)
             result_inst_id_list = []
             # 删除全部
-            _rollback_all(task_id, uop_os_inst_id_list, result_inst_id_list)
+            _rollback_all(resource_id, uop_os_inst_id_list, result_inst_id_list)
             uop_os_inst_id_list = []
 
     return uop_os_inst_id_list
@@ -156,17 +160,17 @@ def _get_ip_from_instance(server):
 
 
 # 回滚删除全部资源和容器
-def _rollback_all(task_id, uop_os_inst_id_list, result_uop_os_inst_id_list):
+def _rollback_all(resource_id, uop_os_inst_id_list, result_uop_os_inst_id_list):
     nova_client = OpenStack.nova_client
     # fail_list = list(set(uop_os_inst_id_list) - set(result_uop_os_inst_id_list))
     fail_list = _uop_os_list_sub(uop_os_inst_id_list, result_uop_os_inst_id_list)
-    Log.logger.debug("Task ID " + task_id.__str__() + " have one or more instance create failed." +
+    Log.logger.debug("Resource ID " + resource_id.__str__() + " have one or more instance create failed." +
                      " Successful instance id set is " + result_uop_os_inst_id_list[:].__str__() +
                      " Failed instance id set is " + fail_list[:].__str__())
     # 删除全部，完成rollback
     for uop_os_inst_id in uop_os_inst_id_list:
         nova_client.servers.delete(uop_os_inst_id['os_inst_id'])
-    Log.logger.debug("Task ID " + task_id.__str__() + " rollback done.")
+    Log.logger.debug("Resource ID " + resource_id.__str__() + " rollback done.")
 
 
 # _uop_os_list_sub
@@ -206,7 +210,8 @@ def _query_resource_set_status(task_id=None, result_list=None, uop_os_inst_id_li
     uop_os_inst_id_wait_query = _uop_os_list_sub(uop_os_inst_id_list, result_uop_os_inst_id_list)
 
     Log.logger.debug("Query Task ID "+task_id.__str__()+", remain "+uop_os_inst_id_wait_query[:].__str__())
-    Log.logger.debug("Test Task Scheduler Class result_uop_os_inst_id_list object id is " + id(result_uop_os_inst_id_list).__str__() +
+    Log.logger.debug("Test Task Scheduler Class result_uop_os_inst_id_list object id is " +
+                     id(result_uop_os_inst_id_list).__str__() +
                      ", Content is " + result_uop_os_inst_id_list[:].__str__())
     nova_client = OpenStack.nova_client
     for uop_os_inst_id in uop_os_inst_id_wait_query:
@@ -241,7 +246,7 @@ def _query_resource_set_status(task_id=None, result_list=None, uop_os_inst_id_li
                 req_dict["redis_ip"] = info["ip"]
             if info["uop_inst_id"] == req_dict["mongodb_inst_id"]:
                 req_dict["mongodb_ip"] = info["ip"]
-        request_res_callback(req_dict)
+        request_res_callback(STATUS_OK, req_dict)
         Log.logger.debug("Call UOP CallBack Post Success Info.")
         TaskManager.task_exit(task_id)
 
@@ -251,6 +256,7 @@ def _query_resource_set_status(task_id=None, result_list=None, uop_os_inst_id_li
         _rollback_all(task_id, uop_os_inst_id_list, result_uop_os_inst_id_list)
 
         # TODO(thread exit): 执行失败调用UOP CallBack停止定时任务退出任务线程
+        request_res_callback(STATUS_FAIL, req_dict)
         Log.logger.debug("Call UOP CallBack Post Fail Info.")
 
         # 停止定时任务并退出
@@ -258,7 +264,7 @@ def _query_resource_set_status(task_id=None, result_list=None, uop_os_inst_id_li
 
 
 # request UOP res_callback
-def request_res_callback(req_dict):
+def request_res_callback(status, req_dict):
     # project_id, resource_name,under_name, resource_id, domain,
     # container_name, image_addr, stardand_ins,cpu, memory, ins_id,
     # mysql_username, mysql_password, mysql_port, mysql_ip,
@@ -267,79 +273,85 @@ def request_res_callback(req_dict):
     """
     :param req_dict: req字段字典
     API JOSN:
-    {
-        "project_id": "项目id",
-        "project_name": [
-            {
-                "resource_name": "资源名称",
-                "under_name": "所属项目",
-                "resource_id": "资源id",
-                "domain": "域名",
-                "container": {
-                    "container_name": "容器名称",
-                    "image_addr": "镜像地址",
-                    "stardand_ins": "实例规格",
-                    "cpu": "2",
-                    "memory": "4",
-                    "ins_id": "实例id"
-                },
-                "db_info": {
-                    "mysql": {
-                        "username": "数据库名",
-                        "password": "密码",
-                        "port": "端口",
-                        "ip": ""
-                    },
-                    "redis": {
-                        "username": "数据库名",
-                        "password": "密码",
-                        "port": "端口",
-                        "ip": ""
-                    },
-                    "mongodb": {
-                        "username": "数据库名",
-                        "password": "密码",
-                        "port": "端口",
-                        "ip": ""
-                    }
-                }
-            }
-        ]
+{
+    "resource_id": "资源id",
+    "resource_name": "资源名",
+    "under_id": "所属部署单元id",
+    "domain": "qitoon.syswin.com",
+    "status": "成功",
+    "container": {
+        "username": "root",
+        "password": "123456",
+        "ip": "容器IP",
+        "container_name": "容器名称",
+        "image_addr": "镜像地址",
+        "stardand_ins": "实例规格",
+        "cpu": "2",
+        "memory": "4",
+        "ins_id": "实例id"
+    },
+    "db_info": {
+        "mysql": {
+            "ins_id": "mysql_inst_id",
+            "username": "数据库名",
+            "password": "密码",
+            "port": "端口",
+            "ip": "MySQLIP"
+        },
+        "redis": {
+            "ins_id": "redis_inst_id",
+            "username": "数据库名",
+            "password": "密码",
+            "port": "端口",
+            "ip": "RedisIP"
+        },
+        "mongodb": {
+            "ins_id": "mongodb_inst_id",
+            "username": "数据库名",
+            "password": "密码",
+            "port": "端口",
+            "ip": "MongodbIP"
+        }
     }
+}
     """
     data = {}
-    data["project_id"] = req_dict["project_id"]
-
-    project_name_list = []
-    project_name = {}
-    project_name["resource_name"] = req_dict["resource_name"]
-    project_name["under_name"] = req_dict["under_name"]
-    project_name["resource_id"] = req_dict["resource_id"]
-    project_name["domain"] = req_dict["domain"]
+    data["resource_id"] = req_dict["resource_id"]
+    data["resource_name"] = req_dict["resource_name"]
+    data["env"] = req_dict["env"]
+    data["under_id"] = req_dict["under_id"]
+    data["domain"] = req_dict["domain"]
+    data["status"] = status
 
     container = {}
+    container["username"] = req_dict["container_username"]
+    container["password"] = req_dict["container_password"]
+    container["ip"] = req_dict["container_ip"]
     container["container_name"] = req_dict["container_name"]
     container["image_addr"] = req_dict["image_addr"]
     container["stardand_ins"] = req_dict["stardand_ins"]
     container["cpu"] = req_dict["cpu"]
     container["memory"] = req_dict["memory"]
     container["ins_id"] = req_dict["container_inst_id"]
-    project_name["container"] = container
+    data["container"] = container
 
     db_info = {}
     mysql = {}
+    mysql["ins_id"] = req_dict["mysql_inst_id"]
     mysql["username"] = req_dict["mysql_username"]
     mysql["password"] = req_dict["mysql_password"]
     mysql["port"] = req_dict["mysql_port"]
     mysql["ip"] = req_dict["mysql_ip"]
 
     redis = {}
+    redis["ins_id"] = req_dict["redis_inst_id"]
     redis["username"] = req_dict["redis_username"]
     redis["password"] = req_dict["redis_password"]
     redis["port"] = req_dict["redis_port"]
     redis["ip"] = req_dict["redis_ip"]
 
     mongodb = {}
+    mongodb["ins_id"] = req_dict["mongodb_inst_id"]
     mongodb["username"] = req_dict["mongodb_username"]
     mongodb["password"] = req_dict["mongodb_password"]
     mongodb["port"] = req_dict["mongodb_port"]
@@ -351,11 +363,7 @@ def request_res_callback(req_dict):
         db_info["redis"] = redis
     if mongodb["ip"] is not IP_NONE:
         db_info["mongodb"] = mongodb
-    project_name["db_info"] = db_info
-
-    project_name_list.append(project_name)
-
-    data["project_name_list"] = project_name_list
+    data["db_info"] = db_info
 
     data_str = json.dumps(data)
     Log.logger.debug("UOP res_callback Request Body is: "+data_str)
@@ -370,98 +378,86 @@ class ResourceSet(Resource):
     @classmethod
     def post(cls):
         parser = reqparse.RequestParser()
+        parser.add_argument('resource_id', type=str)
         parser.add_argument('resource_name', type=str)
-        parser.add_argument('project', type=str)
-        parser.add_argument('project_id', type=str)
-        parser.add_argument('department', type=str)
-        # parser.add_argument('department_id', type=str)
-        parser.add_argument('res_id', type=str)
-        parser.add_argument('user_name', type=str)
-        parser.add_argument('user_id', type=str)
-        parser.add_argument('domain', type=str)
         parser.add_argument('env', type=str)
-        parser.add_argument('application_status', type=str)
-        # parser.add_argument('approval_status', type=str)
+        parser.add_argument('under_id', type=str)
+        parser.add_argument('domain', type=str)
         parser.add_argument('resource_list', type=list, location='json')
         parser.add_argument('compute_list', type=list, location='json')
         args = parser.parse_args()
 
         req_dict = {}
 
+        resource_id = args.resource_id
         resource_name = args.resource_name
-        project = args.project
-        project_id = args.project_id
-        department = args.department
-        department_id = '1'
-        res_id = args.res_id
-        # res_id = str(uuid.uuid1())
-        user_name = args.user_name
-        user_id = args.user_id
-        domain = args.domain
         env = args.env
-        application_status = args.application_status
-        # approval_status = args.approval_status
+        under_id = args.under_id
+        domain = args.domain
         resource_list = args.resource_list
         compute_list = args.compute_list
 
         for resource in resource_list:
-            ins_name = resource.get('res_name')
-            ins_id = resource.get('res_id')
-            # ins_id = str(uuid.uuid1())
-            ins_type = resource.get('res_type')
+            instance_name = resource.get('instance_name')
+            instance_id = resource.get('instance_id')
+            instance_type = resource.get('instance_type')
             cpu = resource.get('cpu')
             mem = resource.get('mem')
             disk = resource.get('disk')
             quantity = resource.get('quantity')
             version = resource.get('version')
-            if ins_type == 'mysql':
-                req_dict["mysql_inst_id"] = ins_id
-            if ins_type == 'redis':
-                req_dict["redis_inst_id"] = ins_id
-            if ins_type == 'mongo':
-                req_dict["mongodb_inst_id"] = ins_id
+
+            if instance_type == 'mysql':
+                req_dict["mysql_inst_id"] = instance_id
+            if instance_type == 'redis':
+                req_dict["redis_inst_id"] = instance_id
+            if instance_type == 'mongo':
+                req_dict["mongodb_inst_id"] = instance_id
 
         for compute in compute_list:
-            ins_name = compute.get('ins_name')
-            ins_id = compute.get('ins_id')
-            # ins_id = str(uuid.uuid1())
+            instance_name = compute.get('instance_name')
+            instance_id = compute.get('instance_id')
             cpu = compute.get('cpu')
             mem = compute.get('mem')
-            url = compute.get('url')
-            req_dict["container_name"] = ins_name
-            req_dict["image_addr"] = url
+            image_url = compute.get('image_url')
+
+            req_dict["container_name"] = instance_name
+            req_dict["image_addr"] = image_url
             req_dict["stardand_ins"] = "2C4G"
             req_dict["cpu"] = cpu
             req_dict["memory"] = mem
-            req_dict["container_inst_id"] = ins_id
+            req_dict["container_inst_id"] = instance_id
 
         Log.logger.debug(resource_list)
         Log.logger.debug(compute_list)
 
-        req_dict["project_id"] = project_id
+        req_dict["resource_id"] = resource_id
         req_dict["resource_name"] = resource_name
-        req_dict["under_name"] = project
-        req_dict["resource_id"] = res_id
+        req_dict["env"] = env
+        req_dict["under_id"] = under_id
         req_dict["domain"] = domain
 
         # init default data
-        req_dict["mysql_username"] = "root"
-        req_dict["mysql_password"] = "123456"
+        req_dict["container_username"] = DEFAULT_USERNAME
+        req_dict["container_password"] = DEFAULT_PASSWORD
+        req_dict["container_ip"] = IP_NONE
+        req_dict["mysql_username"] = DEFAULT_USERNAME
+        req_dict["mysql_password"] = DEFAULT_PASSWORD
         req_dict["mysql_port"] = "3316"
         req_dict["mysql_ip"] = IP_NONE
-        req_dict["redis_username"] = "root"
-        req_dict["redis_password"] = "123456"
+        req_dict["redis_username"] = DEFAULT_USERNAME
+        req_dict["redis_password"] = DEFAULT_PASSWORD
         req_dict["redis_port"] = "6379"
         req_dict["redis_ip"] = IP_NONE
-        req_dict["mongodb_username"] = "root"
-        req_dict["mongodb_password"] = "123456"
+        req_dict["mongodb_username"] = DEFAULT_USERNAME
+        req_dict["mongodb_password"] = DEFAULT_PASSWORD
         req_dict["mongodb_port"] = "27017"
         req_dict["mongodb_ip"] = IP_NONE
 
-        uop_os_inst_id_list = _create_resource_set(res_id, resource_list, compute_list)
+        uop_os_inst_id_list = _create_resource_set(resource_id, resource_list, compute_list)
         if uop_os_inst_id_list.__len__() == 0:
             # TODO(callback): 执行失败调用UOP CallBack
-            pass
+            request_res_callback(STATUS_FAIL, req_dict)
         else:
             # TODO(TaskManager.task_start()): 定时任务示例代码
             result_list = []
