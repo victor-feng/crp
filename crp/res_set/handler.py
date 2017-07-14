c9a64a0de905a5cbbb2592f6b7798ce4792f6155
victor feng <fengyukai@syswin.com>
2017-07-14 10:51:59 +0800

    添加日志

--- crp/res_set/handler.py
+++ crp/res_set/handler.py
@@ -189,7 +189,7 @@ class ResourceProvider(object):
                 nip = '172.28.20.98'
                 port = '8081 9999 1010'  # TODO 前端传值
                 print 'domain&ip:', domain, real_ip
-                Log.logger.debug('the receive domain and ip is %s-%s' % (domain, real_ip))
+                Log.logger.debug('the receive domain and ip port is %s-%s-%s' % (domain, real_ip, ports))
                 self.do_push_nginx_config({'nip': nip, 'domain': domain, 'ip': real_ip.strip(), 'port': ports.strip()})
                 # self.do_push_nginx_config({'nip': nip, 'domain': 'tttttt', 'ip': 'nnnnnnnn'})
                 self.success()
