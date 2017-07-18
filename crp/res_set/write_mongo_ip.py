f5f8b0af34b0313253f28e0bd3cbdb5887c383f7
victor feng <fengyukai@syswin.com>
2017-07-18 21:45:43 +0800

    写mongo ip 文件修改

--- crp/res_set/write_mongo_ip.py
+++ crp/res_set/write_mongo_ip.py
@@ -6,9 +6,20 @@ def write_mongo():
     print sys.argv
     if len(sys.argv) < 2:
         print 'please input the ip'
-    ip = sys.argv[1]
+    try:
+        master_ip = sys.argv[1]
+        slave1 = sys.argv[2]
+        slave2 = sys.argv[3]
+    except IndexError as e:
+        print e
     with open('/tmp/mongo.txt', 'w') as f:
-        f.write(ip)
+        f.write('%s\n' % 'mongomaster')
+        f.write('%s\n' % master_ip)
+        f.write('%s\n' % 'mongoslave1')
+        f.write('%s\n' % slave1)
+        f.write('%s\n' % 'mongoslave2')
+        f.write('%s\n' % slave2)
+
 
 if __name__ == '__main__':
     write_mongo()
