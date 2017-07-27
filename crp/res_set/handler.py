eda5129d64b9b31c1dc050acb75253caab5a6748
wuzhaoyuan <1287597104@qq.com>
2017-07-27 16:53:11 +0800

    增加dns的调用3

--- crp/res_set/handler.py
+++ crp/res_set/handler.py
@@ -144,6 +144,7 @@ class ResourceProviderTransitions(object):
         self.result_inst_id_list = []
         self.uop_os_inst_id_list = []
         self.property_mappers_list = copy.deepcopy(property_mappers_list)
+        self.dns_mappers_list = copy.deepcopy(property_mappers_list)
         self.property_mappers_list.reverse()
         self.push_mappers_list = []
         # 结果集
@@ -582,8 +583,8 @@ class ResourceProviderTransitions(object):
 
         #添加dns操作#
         ip = DNS_ENV.get(self.req_dict["env"])
-        Log.logger.debug("self.push_mappers_list: %s" % self.push_mappers_list)
-        for my_instance in self.push_mappers_list:
+        Log.logger.debug("self.dns_mappers_list: %s" % self.dns_mappers_list)
+        for my_instance in self.dns_mappers_list:
             domain_name = my_instance.get('app',{}).get('domain',{})
             Log.logger.debug('dns add -->ip:%s,domain:%s' %(ip, domain_name))
             self.do_dns_push(domain_name=domain_name, ip=ip)
