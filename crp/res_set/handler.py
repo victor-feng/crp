ef9d7c9870c8c5dbd350d372c2d8ed69033a4c8d
victor feng <fengyukai@syswin.com>
2017-08-03 17:23:37 +0800

    create nginx cluster

--- crp/res_set/handler.py
+++ crp/res_set/handler.py
@@ -24,87 +24,23 @@ resource_set_api = Api(resource_set_blueprint, errors=resource_set_errors)
 
 TIMEOUT = 500
 SLEEP_TIME = 3
+cluster_type_image_port_mappers = DevelopmentConfig.cluster_type_image_port_mappers
+FLAVOR_1C2G = DevelopmentConfig.FLAVOR_1C2G
+DOCKER_FLAVOR_2C4G = DevelopmentConfig.DOCKER_FLAVOR_2C4G
+AVAILABILITY_ZONE_AZ_UOP = DevelopmentConfig.AVAILABILITY_ZONE_AZ_UOP
+DEV_NETWORK_ID = DevelopmentConfig.DEV_NETWORK_ID
+OS_EXT_PHYSICAL_SERVER_ATTR = DevelopmentConfig.OS_EXT_PHYSICAL_SERVER_ATTR
+RES_CALLBACK = DevelopmentConfig.RES_CALLBACK
+RES_STATUS_OK = DevelopmentConfig.RES_STATUS_OK
+RES_STATUS_FAIL = DevelopmentConfig.RES_STATUS_FAIL
+RES_STATUS_DEFAULT = DevelopmentConfig.RES_STATUS_DEFAULT
+DEFAULT_USERNAME = DevelopmentConfig.DEFAULT_USERNAME
+DEFAULT_PASSWORD = DevelopmentConfig.DEFAULT_PASSWORD
+items_sequence_list_config = DevelopmentConfig.items_sequence_list_config
+property_json_mapper_config = DevelopmentConfig.property_json_mapper_config
+SCRIPTPATH = DevelopmentConfig.SCRIPTPATH
+DNS_ENV = DevelopmentConfig.DNS_ENV
 
-cluster_type_image_port_mappers = {
-    'mysql': {
-        'uuid': '817d3140-0b82-4722-9816-3cee734f22b6',
-        'name': 'mysqluop-80G-20170426',
-        'port': '3316'
-    },
-    'redis': {
-        'uuid': '3da55e5b-814c-4935-abf0-1469ae606286',
-        'name': 'redis-50G-20170428',
-        'port': '6379'
-    },
-    'mongodb': {
-        'uuid': '95863650-6816-4588-846a-c0423b5baae0',
-        'name': 'mongosas-50G-20170428',
-        'port': '27017'
-    },
-    'mycat': {
-        'uuid': '59a5022b-3c46-47ec-8e97-b63edc4b7be0',
-        'name': 'mycat-50G-20170628',
-        'port': '3316'
-    }
-}
-
-# scm2-dev--1C2G80G
-FLAVOR_1C2G = 'scm2-dev--1C2G80G'
-# docker-2C4G25G
-DOCKER_FLAVOR_2C4G = 'e90d8d25-c5c7-46d7-ba4e-2465a5b1d266'
-# AVAILABILITY_ZONE
-AVAILABILITY_ZONE_AZ_UOP = 'AZ_UOP'
-DEV_NETWORK_ID = '7aca50a9-cf4b-4cc7-b078-be055dd7c6af'
-OS_EXT_PHYSICAL_SERVER_ATTR = 'OS-EXT-SRV-ATTR:host'
-
-# res_callback
-RES_CALLBACK = 'http://uop-dev.syswin.com/api/res_callback/res'
-
-RES_STATUS_OK = "ok"
-RES_STATUS_FAIL = "fail"
-RES_STATUS_DEFAULT = 'unreserved'
-
-DEFAULT_USERNAME = "root"
-DEFAULT_PASSWORD = "123456"
-
-# Define Request JSON Format
-items_sequence_list_config = [
-    {
-        'compute_list':
-            [
-                'app_cluster'
-            ],
-        'resource_list':
-            [
-                'resource_cluster'
-            ]
-    }]
-
-# Define Item Property to JSON Property Mapper
-property_json_mapper_config = {
-    'app_cluster': {
-        'cluster_name': 'instance_name',
-        'cluster_id': 'instance_id',
-        'domain': 'domain',
-        'image_url': 'image_url',
-        'cpu': 'cpu',
-        'mem': 'mem',
-        'port': 'port',
-        'quantity': 'quantity'
-    },
-    'resource_cluster': {
-        'cluster_name': 'instance_name',
-        'cluster_id': 'instance_id',
-        'cluster_type': 'instance_type',
-        'version': 'version',
-        'cpu': 'cpu',
-        'mem': 'mem',
-        'disk': 'disk',
-        'quantity': 'quantity'
-    }
-}
-SCRIPTPATH = r'crp/res_set/playbook-0830/'
-DNS_ENV = {'develop': '172.28.5.21', 'test': '172.28.18.212'}
 # Transition state Log debug decorator
 def transition_state_logger(func):
     def wrapper(self, *args, **kwargs):
