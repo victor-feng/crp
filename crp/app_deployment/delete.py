a16f6521fe29751172c9a29f04a2dff184546e93
root <xuran@syswin.com>
2017-09-19 11:33:33 +0800

    add delete nginx profiles api

---
+++ crp/app_deployment/delete.py
@@ -0,0 +1,15 @@
+import sys
+import os
+import subprocess
+def delete():
+    print sys.argv
+    domain = sys.argv[1]
+    print domain
+    filepath = "/usr/local/nginx/conf/servers_systoon/" + domain
+    if os.path.exists(filepath):
+        print "will delete %s" % filepath
+    else:
+        return "%s already deleted" % filepath
+    subprocess.Popen('rm -fr %s' %  filepath, shell=True, stdout=subprocess.PIPE)
+    subprocess.Popen('/usr/local/nginx/sbin/nginx -s reload', shell=True, stdout=subprocess.PIPE)
+delete()
