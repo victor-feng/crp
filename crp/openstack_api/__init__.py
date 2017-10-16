89408f9e62e5d1e858e5aa636812186da83ba931
杨阳 <yangyang@syswin.com>
2017-10-16 15:54:26 +0800

    blue

--- crp/openstack_api/__init__.py
+++ crp/openstack_api/__init__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 from flask import Blueprint
 
-az_blueprint = Blueprint('openstack_blueprint', __name__)
+openstack_blueprint = Blueprint('openstack_blueprint', __name__)
 
 from . import handler, forms, errors
