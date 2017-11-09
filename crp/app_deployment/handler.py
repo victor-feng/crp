566311df1a21583f76776758d86b4998285dc2de
杨阳 <yangyang@syswin.com>
2017-11-09 17:52:31 +0800

    12341

--- crp/app_deployment/handler.py
+++ crp/app_deployment/handler.py
@@ -329,7 +329,7 @@ class AppDeploy(Resource):
             if set_flag=="reduce" and appinfo:
                 deploy_msg = "nginx缩减缩容docker完成"
                 _dep_detail_callback(deploy_id, "deploy_increate_nginx",set_flag,deploy_msg)
-                deploy_msg = "扩容完成"
+                deploy_msg = "缩容完成"
                 _dep_detail_callback(deploy_id, "deploy", set_flag, deploy_msg)
             elif set_flag=="reduce" and not appinfo:
                 deploy_msg = "缩容完成"
