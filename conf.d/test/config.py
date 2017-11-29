6f3cdf06049670501cbd0200d4eb22d4d774df2c
杨阳 <yangyang@syswin.com>
2017-11-29 11:36:15 +0800

    fix bug

--- conf.d/test/config.py
+++ conf.d/test/config.py
@@ -104,6 +104,7 @@ class TestingConfig(BaseConfig):
 
     HEALTH_CHECK_PORT = "8000"
     HEALTH_CHECK_PATH = "admin/health"
+    OS_DOCKER_LOGS = "/os_docker_logs"
 
     # Define Request JSON Format
     items_sequence_list_config = [
