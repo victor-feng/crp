91ecd019e79a5d7cd2747b7a487c0eab154bf104
root <xuran@syswin.com>
2017-12-19 14:38:43 +0800

    fix ZONE dict

--- conf.d/prod/config.py
+++ conf.d/prod/config.py
@@ -77,8 +77,8 @@ class ProdConfig(BaseConfig):
     AVAILABILITY_ZONE = {
         "dev": 'AZ_UOP',
         "test": 'AZ_UOP',
-        "prep": 'AZ_UOP',
-        "prod": 'AZ_UOP',
+        "prep": 'AZ-UOP',
+        "prod": 'AZ-UOP',
     }
     DEV_NETWORK_ID = '04f4f74e-a727-48fc-9871-a55c8ae01ea5'
     OS_EXT_PHYSICAL_SERVER_ATTR = 'OS-EXT-SRV-ATTR:host'
