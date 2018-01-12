d0dd4d893a191240a228366570ee2a98c7341929
杨阳 <yangyang@syswin.com>
2018-01-12 11:43:34 +0800

    cloud2.0 最新版本openstack修改

--- crp/openstack.py
+++ crp/openstack.py
@@ -1,14 +1,14 @@
 # -*- coding: utf-8 -*-
 
-from flask import current_app
 
-from novaclient.v1_1 import client as nova_client
-from cinderclient.v1 import client as cinder_client
+
+from keystoneauth1.identity import v2
+from keystoneauth1.identity import v3
+from keystoneauth1 import session
+from novaclient import client as nova_client
 from neutronclient.neutron import client as neutron_client
-# from keystoneclient.v2_0 import client as keystone_client
-from keystoneclient.auth.identity import v2 as v2_auth
-from keystoneclient import session
-from glanceclient.v1 import client as glance_client
+from cinderclient import client as cinder_client
+from glanceclient import client as glance_client
 from config import APP_ENV, configs
 
 
@@ -21,39 +21,16 @@ def openstack_client_setting():
     info = AuthInfo(OPENRC_PATH)
     info.get_env(info.rc)
     OpenStack.auth_info = info
-    OpenStack.nova_client = nova_client.Client(username=info.user_name, api_key=info.user_password,
+    OpenStack.nova_client = nova_client.Client(username=info.user_name, password=info.user_password,
                                                project_id=info.tenant_name, auth_url=info.auth_url)
     # OpenStack.keystone_client = keystone_client.Client(username=info.user_name, password=info.user_password,
     #                                                    tenant_name=info.tenant_name, auth_url=info.auth_url)
     OpenStack.neutron_client = neutron_client.Client('2.0', username=info.user_name, password=info.user_password,
                                                      tenant_name=info.tenant_name, auth_url=info.auth_url)
-    OpenStack.cinder_client = cinder_client.Client(username=info.user_name, api_key=info.user_password,
-                                                   project_id=info.tenant_name, auth_url=info.auth_url)
+    OpenStack.cinder_client = cinder_client.Client('1.0',info.user_name, info.user_password,
+                                                   info.tenant_name, info.auth_url)
     OpenStack.cinder_client.format = 'json'
 
-    # # just for glance now.
-    # def get_endpoint():
-    #     service_list = OpenStack.keystone_client.services.list()
-    #     for i in range(len(service_list)):
-    #         if service_list[i].type == "image":
-    #             glance_id = service_list[i].id
-    #     if glance_id == None:
-    #         return
-    #     endpoint_list = OpenStack.keystone_client.endpoints.list()
-    #     for j in range(len(endpoint_list)):
-    #         if endpoint_list[j].service_id == glance_id:
-    #             return endpoint_list[j].publicurl
-    #
-    # glance_endpoint = get_endpoint()
-    # OpenStack.glance_client = glance_client.Client(endpoint=glance_endpoint, token=OpenStack.keystone_client.auth_token)
-
-    # g_client = OpenStack.glance_client()
-    # filters = {'name': 'trove-centos6-liberty-mm-docker-29'}
-    # kwargs = {'filters': filters}
-    # images = g_client.images.list(**kwargs)
-    # for item in images:
-    #     print type(item)
-    #     print item
 
 
 class AuthInfo(object):
@@ -106,7 +83,7 @@ class OpenStack(object):
     def auth_info(self, value):
         if value is not None:
             OpenStack.auth_i = value
-
+    '''
     @property
     def nova_client(self):
         if OpenStack.nova_c is not None:
@@ -116,7 +93,7 @@ class OpenStack(object):
     def nova_client(self, value):
         if value is not None:
             OpenStack.nova_c = value
-
+    '''
     @property
     def neutron_client(self):
         if OpenStack.neutron_c is not None:
@@ -126,16 +103,6 @@ class OpenStack(object):
     def neutron_client(self, value):
         if value is not None:
             OpenStack.neutron_c = value
-
-    # @property
-    # def glance_client(self):
-    #     if OpenStack.glance_c is not None:
-    #         return OpenStack.glance_c
-
-    # @glance_client.setter
-    # def glance_client(self, value):
-    #     if value is not None:
-    #         OpenStack.glance_c = value
 
     @property
     def cinder_client(self):
@@ -175,19 +142,14 @@ class OpenStack(object):
             cls, service_type,
             auth_url, username,
             password, tenant_name):
-        kwargs = {
-            'auth_url': auth_url,
-            'username': username,
-            'password': password,
-            'tenant_name': tenant_name,
-        }
-        ks_session = session.Session.construct(kwargs)
-        auth = v2_auth.Password(
-            auth_url=auth_url, username=username,
-            password=password, tenant_name=tenant_name)
-        ks_session.auth = auth
-        token = ks_session.get_token()
-        endpoint = ks_session.get_endpoint(
+        auth = v2.Password(
+            auth_url=auth_url,
+            username=username,
+            password=password,
+            tenant_name=tenant_name)
+        sess = session.Session(auth=auth)
+        token = sess.get_token()
+        endpoint = sess.get_endpoint(
             service_type=service_type, endpoint_type='public')
         return endpoint, token
 
@@ -207,6 +169,17 @@ class OpenStack(object):
 
         if OpenStack.glance_c is not None:
             return OpenStack.glance_c
+    @classmethod
+    def nova_client(cls):
+        if OpenStack.auth_info is not None:
+            auth = v2.Password(auth_url=OpenStack.auth_info.auth_url,
+                               username=OpenStack.auth_info.user_name,
+                               password=OpenStack.auth_info.user_password,
+                               tenant_name=OpenStack.auth_info.tenant_name, )
+            sess = session.Session(auth=auth)
+            OpenStack.nova_c = nova_client.Client("2.0", session=sess)
+        if  OpenStack.nova_c is not None:
+            return OpenStack.nova_c
 
     @classmethod
     def get_cinder_endpoint_and_token(cls):
