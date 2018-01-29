840f5c9fc0e6b18e4264906fdde043c6d9027e42
杨阳 <yangyang@syswin.com>
2018-01-29 10:40:32 +0800

    增加k8s 创建应用fix bug

--- crp/res_set/handler2.py
+++ crp/res_set/handler2.py
@@ -467,8 +467,8 @@ class ResourceProviderTransitions2(object):
                                  IP,
                                  replicas
                                  )
+            Log.logger.info('--------deployment----------%s', deployment)
             K8sDeploymentApi.create_deployment(extensions_v1, deployment,NAMESPACE)
-            Log.logger.info('--------deployment----------%s',deployment)
             for i in range(0, replicas, 1):
                 os_inst_id=deployment_name + '_' + str(i)
                 uopinst_info = {
