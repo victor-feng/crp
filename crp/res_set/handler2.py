4eecb6b896c5efa615f75e90ff032261a57bf592
杨阳 <yangyang@syswin.com>
2018-01-31 11:52:21 +0800

    增加k8s 创建应用 更新应用集群fix bug

--- crp/res_set/handler2.py
+++ crp/res_set/handler2.py
@@ -685,10 +685,10 @@ class ResourceProviderTransitions2(object):
                         quantity = value.get('quantity', 0)
                         instances = value.get('instance',[])
                         for instance in instances:
+                            deployment_name = instance.get(
+                                'os_inst_id').split('@@')[0]
+                            ip = instance.get('ip')
                             for deployment_info in deployment_info_list:
-                                deployment_name=instance.get(
-                                        'os_inst_id').split('@@')[0]
-                                ip = instance.get('ip')
                                 if deployment_name == deployment_info['deployment_name'] and ip != deployment_info["pod_ip"]:
                                     instance['ip'] = deployment_info["pod_ip"]
                                     instance['physical_server'] = deployment_info["node_name"]
