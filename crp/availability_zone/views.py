d8970766721c74108b0a183809c6b3015c6258d1
杨阳 <yangyang@syswin.com>
2018-02-01 17:56:13 +0800

    fix bug

--- crp/availability_zone/views.py
+++ crp/availability_zone/views.py
@@ -6,9 +6,138 @@ from crp.availability_zone import az_blueprint
 from crp.availability_zone.errors import az_errors
 from handler import OpenStack_Api,OpenStack2_Api
 from crp.log import Log
+from crp.openstack import OpenStack
 
 
 az_api = Api(az_blueprint, errors=az_errors)
+
+class AZListAPI(Resource):
+
+    def get(self):
+        azs = []
+        try:
+            nova_cli = OpenStack.nova_client
+            for az_item in nova_cli.availability_zones.list():
+                if az_item.zoneName != 'internal':
+                    Log.logger.debug("az_item.hosts: %s, az_item.zoneName: %s, az_item.zoneState: %s",
+                                  az_item.hosts, az_item.zoneName, az_item.zoneState)
+                    azs.append({
+                        "pool_name": az_item.zoneName,
+                        "hosts": az_item.hosts.keys()
+                    })
+        except Exception as e:
+            Log.logger.error('get az err: %s' % e.args)
+            res = {
+                "code": 400,
+                "result": {
+                    "res": "failed",
+                    "msg": e.args
+                }
+            }
+            return res, 400
+        else:
+            res = {
+                "code": 200,
+                "result": {
+                    "msg": "请求成功",
+                    "res": azs
+                }
+            }
+            return res, 200
+
+
+class HostsListAPI(Resource):
+
+    def get(self):
+        parser = reqparse.RequestParser()
+        parser.add_argument('host', type=str, location='args', action='append')
+
+        args = parser.parse_args()
+        host_set = None
+        if args.host:
+            # Log.logger.debug(type(args.host))
+            Log.logger.debug(type(args.host))
+            host_set = set(args.host)
+        Log.logger.debug('HostsListAPI: query %s' % host_set)
+
+        hosts = []
+        try:
+            nova_cli = OpenStack.nova_client
+            rst = nova_cli.hypervisors.list(detailed=True)
+            Log.logger.debug(len(rst))
+            for host_item in rst:
+                Log.logger.debug(dir(host_item))
+                if host_set is None or\
+                                host_item.hypervisor_hostname in host_set:
+                    hosts.append({
+                        "host_name": host_item.hypervisor_hostname,
+                        "host_ip": host_item.host_ip,
+                        "running_vms": host_item.running_vms,
+                        "vcpu_total": host_item.vcpus,
+                        "vcpu_use": host_item.vcpus_used,
+                        "memory_mb_total": host_item.memory_mb,
+                        "memory_mb_use": host_item.memory_mb_used,
+                        "storage_gb_total": host_item.local_gb,
+                        "storage_gb_use": host_item.local_gb_used,
+                    })
+        except Exception as e:
+            Log.logger.error('get hosts err: %s' % e.args)
+            res = {
+                "code": 400,
+                "result": {
+                    "res": "failed",
+                    "msg": e.message
+                }
+            }
+            return res, 400
+        else:
+            res = {
+                "code": 200,
+                "result": {
+                    "msg": "请求成功",
+                    "res": hosts
+                }
+            }
+            return res, 200
+
+
+class StatisticAPI(Resource):
+
+    def get(self):
+        hypervisors_statistics = {}
+        try:
+            nova_cli = OpenStack.nova_client
+            statistics = nova_cli.hypervisors.statistics()
+            Log.logger.debug(dir(statistics))
+            if statistics:
+                hypervisors_statistics["running_vms"]= statistics.running_vms
+                hypervisors_statistics["vcpu_total"]= statistics.vcpus
+                hypervisors_statistics["vcpu_use"]= statistics.vcpus_used
+                hypervisors_statistics["memory_mb_total"]= statistics.memory_mb
+                hypervisors_statistics["memory_mb_use"]= statistics.memory_mb_used
+                hypervisors_statistics["storage_gb_total"]= statistics.local_gb
+                hypervisors_statistics["storage_gb_use"]= statistics.local_gb_used
+        except Exception as e:
+            Log.logger.error('get hypervisors_statistics err: %s' % e.args)
+            res = {
+                "code": 400,
+                "result": {
+                    "res": "failed",
+                    "msg": e.args
+                }
+            }
+            return res, 400
+        else:
+            res = {
+                "code": 200,
+                "result": {
+                    "msg": "请求成功",
+                    "res": hypervisors_statistics
+                }
+            }
+            return res, 200
+
+
 
 
 class UOPStatisticAPI(Resource):
@@ -59,4 +188,7 @@ class UOPStatisticAPI(Resource):
             }
             return res, 200
 
+az_api.add_resource(AZListAPI, '/azs')
+az_api.add_resource(HostsListAPI, '/getHosts')
+az_api.add_resource(StatisticAPI, '/statistics')
 az_api.add_resource(UOPStatisticAPI, '/uopStatistics')
