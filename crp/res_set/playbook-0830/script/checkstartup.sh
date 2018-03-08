21f76012f26acc75f00ac47c7d80909843b43ed4
杨阳 <yangyang@syswin.com>
2018-03-08 13:42:23 +0800

    fix bug uop 虚拟化云war包部署相关

--- crp/res_set/playbook-0830/script/checkstartup.sh
+++ crp/res_set/playbook-0830/script/checkstartup.sh
@@ -2,7 +2,9 @@
 
    n=0
    while [ "$n" -lt 30 ]; do
-     if cat  /home/java/catalina.out  |grep "Server startup"
+     p=`ps -ef | grep tomcat7_8081 | grep -v grep  |wc -l`
+     r=`cat  /home/java/catalina.out  |grep "Server startup" |wc -l `
+     if [[ $r -gt 0 && $p -eq 1 ]]
      then
          echo "Tomcat startup Finish"
          exit 0
@@ -10,4 +12,4 @@
      sleep 3
      ((n++))
    done
-   exit 1 
+   exit 1
