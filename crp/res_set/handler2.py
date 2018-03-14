8fbc2530160ec37747998efdab1b4a0744961676
杨阳 <yangyang@syswin.com>
2018-03-14 16:41:04 +0800

    fix bug uop 支持 war包部署到容器云功能

--- crp/res_set/handler2.py
+++ crp/res_set/handler2.py
@@ -397,6 +397,8 @@ class ResourceProviderTransitions2(object):
                         self.error_msg = err_msg
                         is_rollback = True
                         return is_rollback, uop_os_inst_id_list
+                    else:
+                        image_url = img_url
                 #创建容器云
                 if self.set_flag == "res":
                     service_name = deployment_name
