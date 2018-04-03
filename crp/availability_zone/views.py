346f7149c41a41171009bf07d4236a5b755d9d63
victor feng <fengyukai@syswin.com>
2018-04-03 15:22:01 +0800

    add log

--- crp/availability_zone/views.py
+++ crp/availability_zone/views.py
@@ -17,6 +17,7 @@ class AZListAPI(Resource):
         try:
             azs_cloud1 = OpenStack_Api.get_availability_zones()
             azs_cloud2 = OpenStack2_Api.get_availability_zones()
+            Log.logger.debug(azs_cloud1, azs_cloud2)
             azs = azs_cloud1.extend(azs_cloud2)
         except Exception as e:
             Log.logger.error('get az err: %s' % e.args)
