da1a378778780c815da95edb58657a245867ea09
杨阳 <yangyang@syswin.com>
2018-04-19 19:52:33 +0800

    fix bug crp uop other resource

--- crp/res_set/handler.py
+++ crp/res_set/handler.py
@@ -164,24 +164,13 @@ class ResourceProviderTransitions(object):
         elif self.phase == 'push':
             self.preload_property_mapper(self.push_mappers_list)
 
-        if len(self.property_mapper) != 0 and self.property_mapper.keys()[0] not in ["kvm",self.resource_type]:
-            Log.logger.info("1111111111333311444444444444111111112222222211111111111155555555555551--{}".format(
-                self.property_mapper))
-            Log.logger.info(
-                "11111111113333114444444444441111111122222222111111111111555555555555519999--{}".format(
-                    self.property_mapper.keys()))
+        if len(self.property_mapper) != 0 and self.property_mapper.keys()[0] not in ["kvm"]:
             item_id = self.property_mapper.keys()[0]
-            Log.logger.info("111111111133331144444444444411111111222222221111111111111--{}".format(item_id))
-            Log.logger.info("11111111111111111111222222221111111111111--{}".format(dir(self)))
             if self.phase == 'create':
                 func = getattr(self, item_id, None)
             elif self.phase == 'push':
                 func = getattr(self, ('%s_push' % item_id), None)
-                Log.logger.info("111111111111111111111111111111111--{}".format(func))
-                Log.logger.info("11111111111111111111222222221111111111111--{}".format(dir(self)))
             if not func:
-                Log.logger.info("111111111119999999991111111111111111111111--{}".format(func))
-                Log.logger.info("111111111133331111111111222222221111111111111--{}".format(dir(self)))
                 raise NotImplementedError("Unexpected item_id=%s" % item_id)
             Log.logger.debug('Trigger is %s', item_id)
             func()
