890ed9dfea39cee7a65f5360d5ef5953d39b3f72
杨阳 <yangyang@syswin.com>
2018-04-20 18:14:16 +0800

    fix bug crp uop other resource config

--- crp/res_set/views.py
+++ crp/res_set/views.py
@@ -238,6 +238,7 @@ class ResourceSet(Resource):
         resource_id = args.resource_id
         resource_type = args.resource_type
         resources = deal_del_request_data(resource_id, os_ins_ip_list)
+        resources = resources.get('resources')
         Log.logger.debug("Put resource {}".format(resources))
         try:
             volume_size = volume_size + volume_exp_size
