b6221103c42a42d52d8bf7392a79f1ec55e884ff
杨阳 <yangyang@syswin.com>
2018-05-04 16:27:47 +0800

    fix bug crp uop

--- crp/res_set/handler2.py
+++ crp/res_set/handler2.py
@@ -567,7 +567,7 @@ class ResourceProviderTransitions2(object):
                     }
                     uop_os_inst_id_list.append(uopinst_info)
                     propertys['instance'].append({'host_env': host_env,
-                                                  'instance_type':"app_cluster",
+                                                  'instance_type':host_env,
                                                   'instance_name': instance_name,
                                                   'username': DEFAULT_USERNAME,
                                                   'password': DEFAULT_PASSWORD,
