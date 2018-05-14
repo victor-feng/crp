e902f05a541b018f03f880502ec92eb6bec3e033
杨阳 <yangyang@syswin.com>
2018-05-14 14:40:19 +0800

    fix bug crp uop create kvm add userdata

--- crp/res_set/handler2.py
+++ crp/res_set/handler2.py
@@ -640,9 +640,10 @@ class ResourceProviderTransitions2(object):
                 if cluster_type == "mycat" and quantity > 1:
                     flavor = flavor2 if flavor2 else  KVM_FLAVOR.get("mycat", 'uop-2C4G50G')
                     image_id = image2_id
-                userdata = 'touch /tmp/yangyang.txt'
+                userdata = open("{}/test.sh".format(self.dir))
                 err_msg,osint_id = self._create_instance_by_type(
                     cluster_type, instance_name, flavor, network_id,userdata,image_id, availability_zone,server_group)
+                userdata.close()
                 if not err_msg:
                     uopinst_info = {
                         'uop_inst_id': cluster_id,
