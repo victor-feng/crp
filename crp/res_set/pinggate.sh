80e5d4c3281d12f555cfd41ad5d301eb2fae432e
杨阳 <yangyang@syswin.com>
2018-05-14 16:33:39 +0800

    fix bug crp uop create kvm add userdata

--- crp/res_set/pinggate.sh
+++ crp/res_set/pinggate.sh
@@ -3,7 +3,7 @@
 ip1=`ip addr|grep "scope global"|awk -F ' ' '{print $2}'|awk -F '/' '{print $1}'|awk -F '.' '{print $1}'`
 ip2=`ip addr|grep "scope global"|awk -F ' ' '{print $2}'|awk -F '/' '{print $1}'|awk -F '.' '{print $2}'`
 ip3=`ip addr|grep "scope global"|awk -F ' ' '{print $2}'|awk -F '/' '{print $1}'|awk -F '.' '{print $3}'`
-gateway = `route -n | grep UG | grep -v UGH | awk '{print $2}'`
+gateway=`route -n | grep UG | grep -v UGH | awk '{print $2}'`
 ping $ip1.$ip2.$ip3.1 -c 4
 ping $gateway -c 4
 touch /tmp/uop.txt
