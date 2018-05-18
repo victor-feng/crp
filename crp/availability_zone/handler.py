46afa9a97e05552b5d0f98412d5048bd41e83c53
杨阳 <yangyang@syswin.com>
2018-05-18 15:57:51 +0800

    fix bug crp uop 只有cloud1.0或者cloud2.0 的情况

--- crp/availability_zone/handler.py
+++ crp/availability_zone/handler.py
@@ -4,6 +4,7 @@ from crp.openstack import OpenStack
 from crp.openstack2 import OpenStack as OpenStack2
 from crp.log import Log
 from config import configs, APP_ENV
+from crp.utils.aio import isopenrc
 
 AVAILABILITY_ZONE = configs[APP_ENV].AVAILABILITY_ZONE
 AVAILABILITY_ZONE2 = configs[APP_ENV].AVAILABILITY_ZONE2
@@ -14,6 +15,7 @@ OPENRC2_PATH = configs[APP_ENV].OPENRC2_PATH
 class OpenStack_Api(object):
 
     @classmethod
+    @isopenrc(OPENRC_PATH, res=(0,0,0,0,0,0,0))
     def get_hypervisors_statistics(cls,env):
 
         vcpus = 0
@@ -25,47 +27,45 @@ class OpenStack_Api(object):
         running_vms = 0
         target_zone = None
         try:
-            if OPENRC_PATH:
-                nova_cli = OpenStack.nova_client
-                availability_zones = nova_cli.availability_zones.list()
-                hypervisors = nova_cli.hypervisors.list()
-                for zone in availability_zones:
-                    if AVAILABILITY_ZONE[env] == zone.zoneName:
-                        target_zone = zone
-                        break
-                hosts = []
-                if target_zone:
-                    hosts = target_zone.hosts.keys()
-
-                for hypervisor in hypervisors:
-                    if hypervisor.hypervisor_hostname in hosts:
-                        vcpus = vcpus + hypervisor.vcpus
-                        vcpus_used = vcpus_used + hypervisor.vcpus_used
-                        memory_mb = memory_mb + hypervisor.memory_mb
-                        memory_mb_used = memory_mb_used + hypervisor.memory_mb_used
-                        local_gb = local_gb + hypervisor.local_gb
-                        local_gb_used = local_gb_used + hypervisor.local_gb_used
-                        running_vms = running_vms + hypervisor.running_vms
+            nova_cli = OpenStack.nova_client
+            availability_zones = nova_cli.availability_zones.list()
+            hypervisors = nova_cli.hypervisors.list()
+            for zone in availability_zones:
+                if AVAILABILITY_ZONE[env] == zone.zoneName:
+                    target_zone = zone
+                    break
+            hosts = []
+            if target_zone:
+                hosts = target_zone.hosts.keys()
+            for hypervisor in hypervisors:
+                if hypervisor.hypervisor_hostname in hosts:
+                    vcpus = vcpus + hypervisor.vcpus
+                    vcpus_used = vcpus_used + hypervisor.vcpus_used
+                    memory_mb = memory_mb + hypervisor.memory_mb
+                    memory_mb_used = memory_mb_used + hypervisor.memory_mb_used
+                    local_gb = local_gb + hypervisor.local_gb
+                    local_gb_used = local_gb_used + hypervisor.local_gb_used
+                    running_vms = running_vms + hypervisor.running_vms
         except Exception as e:
             err_msg=str(e)
             Log.logger.error("CRP OpenStack get hypervisors statistics error %s",err_msg)
         return vcpus, vcpus_used, memory_mb, memory_mb_used, local_gb, local_gb_used, running_vms
 
     @classmethod
+    @isopenrc(OPENRC_PATH, res=[])
     def get_availability_zones(cls):
         azs = []
         nova_cli = OpenStack.nova_client
         try:
-            if OPENRC_PATH:
-                for az_item in nova_cli.availability_zones.list():
-                    if az_item.zoneName != 'internal':
-                        Log.logger.debug("az_item.hosts: %s, az_item.zoneName: %s, az_item.zoneState: %s",
-                                      az_item.hosts, az_item.zoneName, az_item.zoneState)
-                        azs.append({
-                            "pool_name": az_item.zoneName,
-                            "hosts": az_item.hosts.keys(),
-                            "cloud": "1"
-                        })
+            for az_item in nova_cli.availability_zones.list():
+                if az_item.zoneName != 'internal':
+                    Log.logger.debug("az_item.hosts: %s, az_item.zoneName: %s, az_item.zoneState: %s",
+                                  az_item.hosts, az_item.zoneName, az_item.zoneState)
+                    azs.append({
+                        "pool_name": az_item.zoneName,
+                        "hosts": az_item.hosts.keys(),
+                        "cloud": "1"
+                    })
 
         except Exception as e:
             err_msg=str(e)
@@ -73,48 +73,48 @@ class OpenStack_Api(object):
         return azs
 
     @classmethod
+    @isopenrc(OPENRC_PATH, res=[])
     def get_hypervisor_hosts(cls, host_set):
 
         hosts = []
         try:
-            if OPENRC_PATH:
-                nova_cli = OpenStack.nova_client
-                rst = nova_cli.hypervisors.list(detailed=True)
-                for host_item in rst:
-                    if host_set is None or \
-                                    host_item.hypervisor_hostname in host_set:
-                        hosts.append({
-                            "host_name": host_item.hypervisor_hostname,
-                            "host_ip": host_item.host_ip,
-                            "running_vms": host_item.running_vms,
-                            "vcpu_total": host_item.vcpus,
-                            "vcpu_use": host_item.vcpus_used,
-                            "memory_mb_total": host_item.memory_mb,
-                            "memory_mb_use": host_item.memory_mb_used,
-                            "storage_gb_total": host_item.local_gb,
-                            "storage_gb_use": host_item.local_gb_used,
-                        })
+            nova_cli = OpenStack.nova_client
+            rst = nova_cli.hypervisors.list(detailed=True)
+            for host_item in rst:
+                if host_set is None or \
+                                host_item.hypervisor_hostname in host_set:
+                    hosts.append({
+                        "host_name": host_item.hypervisor_hostname,
+                        "host_ip": host_item.host_ip,
+                        "running_vms": host_item.running_vms,
+                        "vcpu_total": host_item.vcpus,
+                        "vcpu_use": host_item.vcpus_used,
+                        "memory_mb_total": host_item.memory_mb,
+                        "memory_mb_use": host_item.memory_mb_used,
+                        "storage_gb_total": host_item.local_gb,
+                        "storage_gb_use": host_item.local_gb_used,
+                    })
         except Exception as e:
             err_msg = str(e)
             Log.logger.error("CRP OpenStack get hypervisor hosts error %s", err_msg)
         return hosts
 
     @classmethod
+    @isopenrc(OPENRC_PATH, res={})
     def get_hypervisor_statistics(cls):
 
         hypervisors_statistics = {}
         try:
-            if OPENRC_PATH:
-                nova_cli = OpenStack.nova_client
-                statistics = nova_cli.hypervisors.statistics()
-                if statistics:
-                    hypervisors_statistics["running_vms"] = statistics.running_vms
-                    hypervisors_statistics["vcpu_total"] = statistics.vcpus
-                    hypervisors_statistics["vcpu_use"] = statistics.vcpus_used
-                    hypervisors_statistics["memory_mb_total"] = statistics.memory_mb
-                    hypervisors_statistics["memory_mb_use"] = statistics.memory_mb_used
-                    hypervisors_statistics["storage_gb_total"] = statistics.local_gb
-                    hypervisors_statistics["storage_gb_use"] = statistics.local_gb_used
+            nova_cli = OpenStack.nova_client
+            statistics = nova_cli.hypervisors.statistics()
+            if statistics:
+                hypervisors_statistics["running_vms"] = statistics.running_vms
+                hypervisors_statistics["vcpu_total"] = statistics.vcpus
+                hypervisors_statistics["vcpu_use"] = statistics.vcpus_used
+                hypervisors_statistics["memory_mb_total"] = statistics.memory_mb
+                hypervisors_statistics["memory_mb_use"] = statistics.memory_mb_used
+                hypervisors_statistics["storage_gb_total"] = statistics.local_gb
+                hypervisors_statistics["storage_gb_use"] = statistics.local_gb_used
         except Exception as e:
             err_msg = str(e)
             Log.logger.error("CRP OpenStack get hypervisor statistics error %s", err_msg)
@@ -124,6 +124,7 @@ class OpenStack_Api(object):
 class OpenStack2_Api(object):
 
     @classmethod
+    @isopenrc(OPENRC2_PATH, res=(0, 0, 0, 0, 0, 0, 0))
     def get_hypervisors_statistics(cls, env):
 
         vcpus = 0
@@ -135,95 +136,93 @@ class OpenStack2_Api(object):
         running_vms = 0
         target_zone = None
         try:
-            if OPENRC2_PATH:
-                nova_cli = OpenStack2.nova_client
-                availability_zones = nova_cli.availability_zones.list()
-                hypervisors = nova_cli.hypervisors.list()
-                for zone in availability_zones:
-                    if AVAILABILITY_ZONE2[env] == zone.zoneName:
-                        target_zone = zone
-                        break
-                hosts = []
-                if target_zone:
-                    hosts = target_zone.hosts.keys()
-
-                for hypervisor in hypervisors:
-                    if hypervisor.hypervisor_hostname in hosts:
-                        vcpus = vcpus + hypervisor.vcpus
-                        vcpus_used = vcpus_used + hypervisor.vcpus_used
-                        memory_mb = memory_mb + hypervisor.memory_mb
-                        memory_mb_used = memory_mb_used + hypervisor.memory_mb_used
-                        local_gb = local_gb + hypervisor.local_gb
-                        local_gb_used = local_gb_used + hypervisor.local_gb_used
-                        running_vms = running_vms + hypervisor.running_vms
+            nova_cli = OpenStack2.nova_client
+            availability_zones = nova_cli.availability_zones.list()
+            hypervisors = nova_cli.hypervisors.list()
+            for zone in availability_zones:
+                if AVAILABILITY_ZONE2[env] == zone.zoneName:
+                    target_zone = zone
+                    break
+            hosts = []
+            if target_zone:
+                hosts = target_zone.hosts.keys()
+            for hypervisor in hypervisors:
+                if hypervisor.hypervisor_hostname in hosts:
+                    vcpus = vcpus + hypervisor.vcpus
+                    vcpus_used = vcpus_used + hypervisor.vcpus_used
+                    memory_mb = memory_mb + hypervisor.memory_mb
+                    memory_mb_used = memory_mb_used + hypervisor.memory_mb_used
+                    local_gb = local_gb + hypervisor.local_gb
+                    local_gb_used = local_gb_used + hypervisor.local_gb_used
+                    running_vms = running_vms + hypervisor.running_vms
         except Exception as e:
             err_msg=str(e)
             Log.logger.error("CRP OpenStack2 get hypervisors statistics error %s",err_msg)
         return vcpus, vcpus_used, memory_mb, memory_mb_used, local_gb, local_gb_used, running_vms
 
     @classmethod
+    @isopenrc(OPENRC2_PATH, res=[])
     def get_availability_zones(cls):
         azs = []
         nova_cli = OpenStack2.nova_client
         try:
-            if OPENRC2_PATH:
-                for az_item in nova_cli.availability_zones.list():
-                    if az_item.zoneName != 'internal':
-                        Log.logger.debug("az_item.hosts: %s, az_item.zoneName: %s, az_item.zoneState: %s",
-                                      az_item.hosts, az_item.zoneName, az_item.zoneState)
-                        azs.append({
-                            "pool_name": az_item.zoneName,
-                            "hosts": az_item.hosts.keys(),
-                            "cloud": "2"
-                        })
+            for az_item in nova_cli.availability_zones.list():
+                if az_item.zoneName != 'internal':
+                    Log.logger.debug("az_item.hosts: %s, az_item.zoneName: %s, az_item.zoneState: %s",
+                                  az_item.hosts, az_item.zoneName, az_item.zoneState)
+                    azs.append({
+                        "pool_name": az_item.zoneName,
+                        "hosts": az_item.hosts.keys(),
+                        "cloud": "2"
+                    })
         except Exception as e:
             err_msg=str(e)
             Log.logger.error("CRP OpenStack get availability zones error %s",err_msg)
         return azs
 
     @classmethod
+    @isopenrc(OPENRC2_PATH, res=[])
     def get_hypervisor_hosts(cls, host_set):
 
         hosts = []
         try:
-            if OPENRC2_PATH:
-                nova_cli = OpenStack2.nova_client
-                rst = nova_cli.hypervisors.list(detailed=True)
-                for host_item in rst:
-                    if host_set is None or \
-                                    host_item.hypervisor_hostname in host_set:
-                        hosts.append({
-                            "host_name": host_item.hypervisor_hostname,
-                            "host_ip": host_item.host_ip,
-                            "running_vms": host_item.running_vms,
-                            "vcpu_total": host_item.vcpus,
-                            "vcpu_use": host_item.vcpus_used,
-                            "memory_mb_total": host_item.memory_mb,
-                            "memory_mb_use": host_item.memory_mb_used,
-                            "storage_gb_total": host_item.local_gb,
-                            "storage_gb_use": host_item.local_gb_used,
-                        })
+            nova_cli = OpenStack2.nova_client
+            rst = nova_cli.hypervisors.list(detailed=True)
+            for host_item in rst:
+                if host_set is None or \
+                                host_item.hypervisor_hostname in host_set:
+                    hosts.append({
+                        "host_name": host_item.hypervisor_hostname,
+                        "host_ip": host_item.host_ip,
+                        "running_vms": host_item.running_vms,
+                        "vcpu_total": host_item.vcpus,
+                        "vcpu_use": host_item.vcpus_used,
+                        "memory_mb_total": host_item.memory_mb,
+                        "memory_mb_use": host_item.memory_mb_used,
+                        "storage_gb_total": host_item.local_gb,
+                        "storage_gb_use": host_item.local_gb_used,
+                    })
         except Exception as e:
             err_msg = str(e)
             Log.logger.error("CRP OpenStack get hypervisor hosts error %s", err_msg)
         return hosts
 
     @classmethod
+    @isopenrc(OPENRC2_PATH, res={})
     def get_hypervisor_statistics(cls):
 
         hypervisors_statistics = {}
         try:
-            if  OPENRC2_PATH:
-                nova_cli = OpenStack2.nova_client
-                statistics = nova_cli.hypervisors.statistics()
-                if statistics:
-                    hypervisors_statistics["running_vms"] = statistics.running_vms
-                    hypervisors_statistics["vcpu_total"] = statistics.vcpus
-                    hypervisors_statistics["vcpu_use"] = statistics.vcpus_used
-                    hypervisors_statistics["memory_mb_total"] = statistics.memory_mb
-                    hypervisors_statistics["memory_mb_use"] = statistics.memory_mb_used
-                    hypervisors_statistics["storage_gb_total"] = statistics.local_gb
-                    hypervisors_statistics["storage_gb_use"] = statistics.local_gb_used
+            nova_cli = OpenStack2.nova_client
+            statistics = nova_cli.hypervisors.statistics()
+            if statistics:
+                hypervisors_statistics["running_vms"] = statistics.running_vms
+                hypervisors_statistics["vcpu_total"] = statistics.vcpus
+                hypervisors_statistics["vcpu_use"] = statistics.vcpus_used
+                hypervisors_statistics["memory_mb_total"] = statistics.memory_mb
+                hypervisors_statistics["memory_mb_use"] = statistics.memory_mb_used
+                hypervisors_statistics["storage_gb_total"] = statistics.local_gb
+                hypervisors_statistics["storage_gb_use"] = statistics.local_gb_used
         except Exception as e:
             err_msg = str(e)
             Log.logger.error("CRP OpenStack get hypervisor statistics error %s", err_msg)
--- crp/openstack_api/handler.py
+++ crp/openstack_api/handler.py
@@ -43,49 +43,48 @@ class OpenStack_Api(object):
         return name2id
 
     @classmethod
+    @isopenrc(OPENRC_PATH, res=[])
     def get_ports(cls,network_id):
         ports=[]
         try:
-            if OPENRC_PATH:
-                net_cli = OpenStack.neutron_client
-                ports = net_cli.list_ports(**{'network_id': network_id})
-                ports = ports.get('ports',[])
+            net_cli = OpenStack.neutron_client
+            ports = net_cli.list_ports(**{'network_id': network_id})
+            ports = ports.get('ports',[])
         except Exception as e:
             err_msg = str(e)
             Log.logger.error("CRP OpenStack get port info error %s", err_msg)
         return  ports
 
     @classmethod
+    @isopenrc(OPENRC_PATH, res=None)
     def get_vm_status(cls,os_inst_id):
         vm_state = None
         try:
-            if OPENRC_PATH:
-                nova_cli = OpenStack.nova_client
-                vm = nova_cli.servers.get(os_inst_id)
-                vm_state = vm.status.lower()
+            nova_cli = OpenStack.nova_client
+            vm = nova_cli.servers.get(os_inst_id)
+            vm_state = vm.status.lower()
         except Exception as e:
             err_msg = str(e)
             Log.logger.error("CRP OpenStack get vm status error %s", err_msg)
         return  vm_state
 
     @classmethod
-    # @isopenrc(OPENRC_PATH, res={})
+    @isopenrc(OPENRC_PATH, res={})
     def get_all_vm_status(cls):
         vm_info_dict = {}
         try:
-            if OPENRC_PATH:
-                nova_cli = OpenStack.nova_client
-                vms = nova_cli.servers.list()
-                for vm in vms:
-                    os_inst_id = vm.id
-                    ips = vm.networks
-                    if ips:
-                        ip = vm.networks.values()[0][0]
-                    else:
-                        ip = "127.0.0.1"
-                    status = vm.status.lower()
-                    physical_server = getattr(vm, OS_EXT_PHYSICAL_SERVER_ATTR)
-                    vm_info_dict[os_inst_id] = [ip, status,physical_server]
+            nova_cli = OpenStack.nova_client
+            vms = nova_cli.servers.list()
+            for vm in vms:
+                os_inst_id = vm.id
+                ips = vm.networks
+                if ips:
+                    ip = vm.networks.values()[0][0]
+                else:
+                    ip = "127.0.0.1"
+                status = vm.status.lower()
+                physical_server = getattr(vm, OS_EXT_PHYSICAL_SERVER_ATTR)
+                vm_info_dict[os_inst_id] = [ip, status,physical_server]
         except Exception as e:
             err_msg = str(e)
             Log.logger.error("CRP OpenStack get all vm status error %s", err_msg)
@@ -96,77 +95,77 @@ class OpenStack_Api(object):
 class OpenStack2_Api(object):
 
     @classmethod
+    @isopenrc(OPENRC2_PATH, res={})
     def get_network_info(cls):
         subnet_info = {}
         name2id = {}
         try:
-            if OPENRC2_PATH:
-                net_cli = OpenStack2.neutron_client
-                networks = net_cli.list_networks()
-                networks = networks.get('networks', [])
-                subnets = net_cli.list_subnets()["subnets"]
-                for subnet in subnets:
-                    network_id = subnet["network_id"]
-                    sub_vlan=subnet["cidr"]
-                    if network_id in subnet_info.keys():
-                        subnet_info[network_id].append(sub_vlan)
-                    else:
-                        subnet_info[network_id] = [sub_vlan]
-                for network in networks:
-                    name = network.get('name')
-                    id_ = network.get('id')
-                    for network_id in subnet_info.keys():
-                        if network_id == id_:
-                            sub_vlans=subnet_info[network_id]
-                            name2id[name] = [id_,sub_vlans,"2"]
+             net_cli = OpenStack2.neutron_client
+             networks = net_cli.list_networks()
+             networks = networks.get('networks', [])
+             subnets = net_cli.list_subnets()["subnets"]
+             for subnet in subnets:
+                 network_id = subnet["network_id"]
+                 sub_vlan=subnet["cidr"]
+                 if network_id in subnet_info.keys():
+                     subnet_info[network_id].append(sub_vlan)
+                 else:
+                     subnet_info[network_id] = [sub_vlan]
+             for network in networks:
+                 name = network.get('name')
+                 id_ = network.get('id')
+                 for network_id in subnet_info.keys():
+                     if network_id == id_:
+                         sub_vlans=subnet_info[network_id]
+                         name2id[name] = [id_,sub_vlans,"2"]
         except Exception as e:
             err_msg=str(e)
             Log.logger.error("CRP OpenStack2 get network info error %s",err_msg)
         return name2id
 
     @classmethod
+    @isopenrc(OPENRC2_PATH, res=[])
     def get_ports(cls,network_id):
         ports=[]
         try:
-            if  OPENRC2_PATH:
-                net_cli = OpenStack2.neutron_client
-                ports = net_cli.list_ports(**{'network_id': network_id})
-                ports = ports.get('ports',[])
+            net_cli = OpenStack2.neutron_client
+            ports = net_cli.list_ports(**{'network_id': network_id})
+            ports = ports.get('ports',[])
         except Exception as e:
             err_msg = str(e)
             Log.logger.error("CRP OpenStack2 get port info error %s", err_msg)
         return  ports
 
     @classmethod
+    @isopenrc(OPENRC2_PATH, res=None)
     def get_vm_status(cls,os_inst_id):
         vm_state = None
         try:
-            if OPENRC2_PATH:
-                nova_cli = OpenStack2.nova_client
-                vm = nova_cli.servers.get(os_inst_id)
-                vm_state = vm.status.lower()
+            nova_cli = OpenStack2.nova_client
+            vm = nova_cli.servers.get(os_inst_id)
+            vm_state = vm.status.lower()
         except Exception as e:
             err_msg = str(e)
             Log.logger.error("CRP OpenStack2 get vm status error %s", err_msg)
         return  vm_state
 
     @classmethod
+    @isopenrc(OPENRC2_PATH, res={})
     def get_all_vm_status(cls):
         vm_info_dict = {}
         try:
-            if OPENRC2_PATH:
-                nova_cli = OpenStack2.nova_client
-                vms = nova_cli.servers.list()
-                for vm in vms:
-                    os_inst_id = vm.id
-                    ips = vm.networks
-                    if ips:
-                        ip = vm.networks.values()[0][0]
-                    else:
-                        ip = "127.0.0.1"
-                    status = vm.status.lower()
-                    physical_server = getattr(vm, OS_EXT_PHYSICAL_SERVER_ATTR)
-                    vm_info_dict[os_inst_id] = [ip, status,physical_server]
+            nova_cli = OpenStack2.nova_client
+            vms = nova_cli.servers.list()
+            for vm in vms:
+                os_inst_id = vm.id
+                ips = vm.networks
+                if ips:
+                    ip = vm.networks.values()[0][0]
+                else:
+                    ip = "127.0.0.1"
+                status = vm.status.lower()
+                physical_server = getattr(vm, OS_EXT_PHYSICAL_SERVER_ATTR)
+                vm_info_dict[os_inst_id] = [ip, status,physical_server]
         except Exception as e:
             err_msg = str(e)
             Log.logger.error("CRP OpenStack2 get all vm status error %s", err_msg)
