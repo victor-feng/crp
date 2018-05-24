633937f8fb8f8dacb9269fa96636cba8492fbd4e
machunqing <machunqing@syswin.com>
2018-05-24 09:50:51 +0800

    fix nginx template

--- crp/app_deployment/other_config.py
+++ crp/app_deployment/other_config.py
@@ -1,21 +1,94 @@
-#!/usr/bin/python
 # -*- coding: utf-8 -*-
+
 import sys,os
 import re
 import subprocess
-
+import ast
+from jinja2 import Environment, FileSystemLoader
 
 BASE_PATH = "/usr/local/nginx/conf/servers_systoon"
 TEMPLATE_PATH = "/tmp"
-# BASE_PATH = "/home/mcq/tmp"
-# TEMPLATE_PATH = "/home/mcq/tmp"
+# BASE_PATH = "/home/mcq/tlp"
+# TEMPLATE_PATH = "/home/mcq/tlp"
 
 
-def create(**kwargs):
+class Params(object):
     """
-        create config
+        参数
     """
-    # params
+    def __init__(self, domain, project, ips, 
+                        dpath=None, template=None, certificate=None):
+        self.domain = domain
+        self.project = project
+        self.ips = ips if isinstance(ips, list) else [ips]
+        self.dpath = '/' + dpath.strip('/') + '/' if dpath.strip('/') else '/'
+        self.template = template
+        self.certificate = certificate
+
+
+def cover_temp(domain, objs, conf_path):
+    """
+        写配置文件
+    """
+    if not isinstance(objs, list):
+        objs = [objs]
+
+    env = Environment(loader=FileSystemLoader(TEMPLATE_PATH))
+    t = env.get_template(objs[0].template)
+    content = t.render(domain=domain, objs=objs)
+
+    content_list = content.split('\n')
+    txt = [i for i in content_list if i.strip()]
+
+    with open(conf_path, 'wb') as f:
+        f.write('\n'.join(txt))
+
+
+def update_or_extend(new_obj, conf_path):
+    """
+        更新或扩展
+    """
+    with open(conf_path, 'rb') as f:
+        content_list = f.read().split('#-->')
+
+    old_objs = []
+    for i in range(1, len(content_list)):
+        old_objs.append(Params(**ast.literal_eval(content_list[i].strip())))
+
+    for index, obj in enumerate(old_objs):
+        if new_obj.project == obj.project:
+            del old_objs[index]
+            break
+
+    old_objs.append(new_obj)
+    
+    cover_temp(new_obj.domain, old_objs, conf_path)
+
+
+def remove(del_obj, conf_path):
+    with open(conf_path, 'rb') as f:
+        content_list = f.read().split('#-->')
+
+    old_objs = []
+    for i in range(1, len(content_list)):
+        old_objs.append(Params(**ast.literal_eval(content_list[i].strip())))
+
+    if len(old_objs) < 2 and old_objs[0].project == del_obj.project:
+        subprocess.Popen(
+            'rm {conf}'.format(conf=conf_path), shell=True, stdout=subprocess.PIPE)
+        return
+
+    for index, obj in enumerate(old_objs):
+        if del_obj.project == obj.project:
+            del old_objs[index]
+
+    cover_temp(del_obj.domain, old_objs, conf_path)
+
+
+def get_obj(**kwargs):
+    """
+        参数实例化
+    """
     certificate = kwargs.get('-certificate', "")
     project = kwargs.get('-project', "") + ".com"
     domain = kwargs.get('-domain', "")
@@ -23,199 +96,37 @@ def create(**kwargs):
     ip_list = kwargs.get('-ip', "").split(",")
     port = kwargs.get('-port', 80)
 
-    template = '{path}/other_template_https'.format(path=TEMPLATE_PATH) \
-            if certificate else '{path}/other_template_http'.format(path=TEMPLATE_PATH)
+    ips = ['{i}:{port}'.format(i=i, port=port) for i in ip_list]
 
-    if not all([project, domain, ip_list]):
-        print "Error: params value error"; return
+    template = 'other_template_https' if certificate else 'other_template_http'
 
+    obj = Params(domain, project, ips, domain_path, template, certificate)
+    return obj
+
+
+def create(**kwargs):
+    obj = get_obj(**kwargs)
     # dir
     if not os.path.isdir(BASE_PATH):
         subprocess.Popen(
             'mkdir {path}'.format(path=BASE_PATH), shell=True, stdout=subprocess.PIPE)
 
-    nginx_conf = os.path.join(BASE_PATH, domain)
-
-    # params contact
-    ip_port = ['{i}:{port}'.format(i=i, port=port) for i in ip_list]
-    ips = write_server_config(ip_port)
+    nginx_conf = os.path.join(BASE_PATH, obj.domain)
 
     if os.path.exists(nginx_conf):
-        with open(nginx_conf, 'rb') as f:
-            all_c = f.read()
-            content = all_c.split("# --- divid ---")
-
-        # if re.findall(project, content[0]):
-        #     print "Error: project already exists !"; return
-        # if re.findall(project, content[2]):
-        #     print "Error: domain_path already exists !"; return
-
-
-        content[2] = "# --- divid ---" + content[2]
-        tp = sub_content(
-            template, project, ips, domain, domain_path, certificate).split("# --- divid ---")
-
-        if re.findall(project, content[0]):
-            # replace ips
-            tmp = [i.strip() for i in content[0].split('upstream') if project in i]
-            all_c = all_c.replace(tmp[0], tp[0].lstrip('upstream').strip())
-            # all_c = re.sub(tmp[0], tp[0].lstrip('upstream').strip(), all_c)
-            # replace damain_path
-            tmp_l = [i for i in content[2].split('location') if project in i]
-            d_path = tmp_l[0].split('{')[0].strip()
-            if d_path == '/':
-                content = re.sub('location / ', 'location /{d_path}/ '.format(d_path=domain_path), all_c)
-            else:
-                domain_path = '/' + domain_path + '/' if domain_path else '/'
-                content = re.sub(d_path, domain_path, all_c)
-        else:
-            tp = [i + "# --- divid ---" for i in tp]
-            content.insert(1, tp[0])
-            content.insert(4, tp[2])
-            content = "".join(content)
+        update_or_extend(obj, nginx_conf)
     else:
-        content =sub_content(
-            template, project, ips, domain, domain_path, certificate)
-        content = '  ' + content
-
-    fp = open(nginx_conf, 'wb')
-    fp.write(content)
-    fp.close()
-
-    # reload nginx
-    subprocess.Popen('/usr/local/nginx/sbin/nginx -s reload', shell=True, stdout=subprocess.PIPE)
-
-
-# def update(**kwargs):
-#     """
-#         update config
-#     """
-#     certificate = kwargs.get('-certificate', "")
-#     project = kwargs.get('-project', "")
-#     domain = kwargs.get('-domain', "")
-#     domain_path = kwargs.get('-domain_path', "")
-#     ip_list = kwargs.get('-ip', "").split(",")
-#     port = kwargs.get('-port', 80)
-# 
-#     template = '{}/other_template_https'.format(TEMPLATE_PATH) \
-#             if certificate else '{}/other_template_http'.format(TEMPLATE_PATH)
-# 
-#     nginx_conf = os.path.join(BASE_PATH, domain)
-#     if not os.path.exists(nginx_conf):
-#         return "Error: config file not exists"
-# 
-#     ip_port = ['{}:{}'.format(i, port) for i in ip_list]
-#     ips = write_server_config(ip_port)
-# 
-#     if os.path.exists(nginx_conf):
-#         with open(nginx_conf, 'rb') as f:
-#             content = f.read()
-#             fp = content.split("# --- divid ---")
-# 
-#         if not re.findall(project, fp[0]):
-#             return "Error: project not exists !"
-# 
-#         tp = sub_content(
-#             template, project, ips, domain, domain_path, certificate).split("# --- divid ---")
-# 
-#         tmp = [i.strip() for i in fp[0].split('upstream') if project in i]
-#         content = re.sub(tmp[0], tp[0].lstrip('upstream').strip(), content)
-# 
-#     fp = open(nginx_conf, 'wb')
-#     fp.write(content)
-#     fp.close()
-# 
-#     # reload nginx
-#     subprocess.Popen('/usr/local/nginx/sbin/nginx -s reload', shell=True, stdout=subprocess.PIPE)
+        cover_temp(domain, obj, nginx_conf)
 
 
 def delete(**kwargs):
-    """
-        delete config 
-    """
-    project = kwargs.get('-project', "") + '.com'
-    domain = kwargs.get('-domain', "")
+    del_obj = get_obj(**kwargs)
+    nginx_conf = os.path.join(BASE_PATH, del_obj.domain)
 
-    if not os.path.isdir(BASE_PATH):
-        subprocess.Popen(
-            'mkdir {path}'.format(path=BASE_PATH), shell=True, stdout=subprocess.PIPE)
-
-    nginx_conf = os.path.join(BASE_PATH, domain)
     if not os.path.exists(nginx_conf):
         print "File is not exists"; return
 
-    with open(nginx_conf, 'rb') as f:
-        tp = f.read().split("# --- divid ---")
-
-    # logic judgment
-    if project not in tp[0]:
-        print "Error: project not exists"; return
-    if project not in tp[2]:
-        print "Error: domain path not exists"; return
-    if len(filter(lambda x: len(x) > 0, 
-                  tp[0].strip().split('upstream'))) <= 1:
-        subprocess.Popen(
-            'rm {conf}'.format(conf=nginx_conf), shell=True, stdout=subprocess.PIPE)
-        return
-
-    # upstream part
-    result = []
-    for i in tp[0].split('upstream'):
-        if project in i or not i.strip():
-            continue
-        result.append('  upstream' + i.rstrip() + '\n')
-
-    # server part
-    result.append('  # --- divid ---' + tp[1] + '# --- divid ---')
-
-    # location part
-    tmp = []
-    for i in tp[2].split('location'):
-        if project in i or not i.strip():
-            continue
-        tmp.append('\t  location' + i.rstrip() + '\n')
-    result.append(''.join(tmp))
-
-    # final part
-    result.append('\t  # --- divid ---' + tp[3])
-
-    # write file
-    with open(nginx_conf, 'wb') as f:
-        f.write('\n'.join(result))
-    # reload nginx
-    subprocess.Popen('/usr/local/nginx/sbin/nginx -s reload', shell=True, stdout=subprocess.PIPE)
-
-
-def sub_content(template, project, ips, domain, domain_path, certificate):
-    with open(template, 'rb') as f:
-        tp_str = f.read().strip()
-
-    content = re.sub(r'Project', project, tp_str)
-    content = re.sub(
-        r'server  IpPort max_fails=0;', ips, content)
-
-    content = re.sub(r'Domain', domain, content)
-    if domain_path:
-        content = re.sub(r'DPath', domain_path + '/', content)
-    else:
-        content = re.sub(r'DPath', '', content)
-
-    # if "innertoon.com" in domain:
-    #     f_dst3 = re.sub(r' t100SubDomain.innertoon.com','',f_dst5)
-
-    # else:
-    #     f_dst3 = re.sub(r'SubDomain', subdomain, f_dst5)
-
-    if certificate:
-        content = re.sub(r'Certificate', certificate, content)
-    return content
-
-
-def write_server_config(ip_port):
-    content = ''
-    for i in ip_port:
-        content += '\t' + 'server  ' + i + ' max_fails=0'+ ';' + '\n'
-    return content.lstrip('\t').rstrip('\n')
+    remove(del_obj, nginx_conf)
 
 
 if __name__ == '__main__':
@@ -228,7 +139,7 @@ if __name__ == '__main__':
     if os.path.exists(nginx_conf):
         with open(nginx_conf, 'rb') as f:
             fp = f.read()
-        my_domain = re.findall(r'# --- divid ---', fp, flags=re.M)
+        my_domain = re.findall(r'#-->', fp, flags=re.M)
         assert my_domain, "Domain already existed in {p},used in k8s app".format(p=BASE_PATH)
 
     if cmd == '-c':
