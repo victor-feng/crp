a07fa62af61c843b6811343060d63eb4e3b43cec
杨阳 <yangyang@syswin.com>
2018-05-30 09:40:14 +0800

    fix bug crp uop git 构建相关

--- crp/res_set/handler2.py
+++ crp/res_set/handler2.py
@@ -562,11 +562,22 @@ class ResourceProviderTransitions2(object):
 
             elif host_env == "kvm":
                 #创建虚拟化云
+                war_url = ""
+                if deploy_source == "git":
+                    git_url = image_url
+                    err_msg,war_url = git_code_to_war(git_url,branch,self.project_name,pom_path,self.env,language_env)
+                    Log.logger.debug(
+                        "CRP git code to war  err_msg:{err_msg}--------war_url:{war_url}".format(err_msg=err_msg,
+                                                                                                    war_url=war_url))
+                    if err_msg:
+                        self.error_msg = err_msg
+                        is_rollback = True
+                        return is_rollback, uop_os_inst_id_list
                 quantity=replicas
                 userdata = open("{}/pinggate.sh".format(self.dir))
                 is_rollback, uop_os_inst_id_list = self._create_kvm_cluster(property_mapper, cluster_id, host_env,
                                                                             image_id, port, cpu, mem, flavor,
-                                                                            quantity, network_id,userdata, availability_zone,language_env,pom_path,branch)
+                                                                            quantity, network_id,userdata, availability_zone,language_env,war_url)
                 userdata.close()
 
             else:
@@ -575,7 +586,7 @@ class ResourceProviderTransitions2(object):
 
         return is_rollback, uop_os_inst_id_list
 
-    def _create_kvm_cluster(self,property_mapper,cluster_id, host_env,image_id,port,cpu,mem,flavor,quantity,network_id,userdata,availability_zone,language_env,pom_path,branch):
+    def _create_kvm_cluster(self,property_mapper,cluster_id, host_env,image_id,port,cpu,mem,flavor,quantity,network_id,userdata,availability_zone,language_env,war_url=None):
         is_rollback = False
         uop_os_inst_id_list = []
         kvm_tag = time.time().__str__()[6:10]
@@ -616,6 +627,7 @@ class ResourceProviderTransitions2(object):
                                                   'username': DEFAULT_USERNAME,
                                                   'password': DEFAULT_PASSWORD,
                                                   'port': port,
+                                                  'img_url': war_url,
                                                   'os_inst_id': osint_id})
                 else:
                     self.error_msg = err_msg
