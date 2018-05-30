7abcb75ff43954b8acd9f5337d06b75cc6ce4b25
杨阳 <yangyang@syswin.com>
2018-05-30 11:23:33 +0800

    fix bug crp uop git 构建相关

--- crp/utils/git_tools.py
+++ crp/utils/git_tools.py
@@ -97,6 +97,8 @@ def put_war_to_ftp(env,project_name,base_war):
         ftp_user = FTP_USER
         ftp_passwd = FTP_PASSWORD
         ftp = FTP(host=ftp_host, user=ftp_user, passwd=ftp_passwd)
+        Log.logger.info("000000000000000000000000001{}".format(ftp_host))
+        Log.logger.info("1111111111111111111111111111111{}".format(ftp))
         remote_dir = "{ftp_dir}/uop/{project_name}_{env}_{tag}".format(ftp_dir=FTP_DIR,env=env,project_name=project_name,tag=tag)
         ftp.mkd(remote_dir)
         remote_war = os.path.join(remote_dir,war_name)
